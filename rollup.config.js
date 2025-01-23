0bd583047f8d2cf22d2c2dc88aa1a06ad7676007
Akash Kumar <codewithpanda28@gmail.com>
2025-01-23 13:17:59 +0530

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,15 @@
+// rollup.config.js
+import postcss from 'rollup-plugin-postcss';
+
 export default {
-    input: 'src/index.js',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'cjs'
-    },
-    external: ['./calender.css']
-  };
-  
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [
+    postcss({
+      extensions: ['.css']
+    })
+  ]
+};
